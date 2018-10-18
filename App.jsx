bc02bfa1ae72f0dec977e4e5415af9a11fd7288d
Gaetano <gaetano.sabino@gmail.com>
2018-10-18 12:30:07 -0500

    metododos en App.jsx creamos index.html y creamos start en package.json

--- App.jsx
+++ App.jsx
@@ -1,18 +1,37 @@
 import React from 'react';
-import Component from '../Props/Component';
 
 class App extends React.Component{
     constructor(){
         super()
         this.state= {mensaje:'Bienvenido al metodo constructor'}
+        console.log('prima fase del constructor');
+        
     }
 
     render(){
+        console.log('tercera Fase: Metodo Render');
+        
+        let nuemero1=500;
+        let numero2=450;
+        let resultado=nuemero1*numero2;
         return(
             <div>
-                {this.state.mensaje}
+                <h1>{this.state.mensaje}</h1>
+                <div>
+                    <h3>El valor de la moltiplicazione es:{resultado}</h3>
+                </div>
             </div>
         )
+    }
+
+    componentWillMount(){
+        console.log('Bienvenido en componentWillMount');
+        console.log('Segunda Fase: Metodo componentWillMount');        
+    }
+    componentDidMount(){
+        console.log('Cuarta Fase: Metodo componentDidMount');
+        this.setState({mensaje:'Bienvendido al metodo componentDidMount'})
+        
     }
 }
 
